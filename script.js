2f9557fd457ffcb128afde6d573e8e4a3a84845c
Hamksid <hameedlateef343@gmail.com>
2024-05-27 10:32:59 +0100

    my nav

---
+++ script.js
@@ -0,0 +1,18 @@
+var mainMenu = document.querySelector('#mainMenu')
+var closeMenu = document.querySelector('#closeMenu')
+var openMenu = document.querySelector('#openMenu')
+
+
+openMenu.addEventListener('click',show)
+closeMenu.addEventListener('click',close)
+
+function show() {
+  mainMenu.style.display ='flex';
+  mainMenu.style.top ='0';
+}
+function close() {
+  mainMenu.style.top ='-100%';
+}
+
+
+
